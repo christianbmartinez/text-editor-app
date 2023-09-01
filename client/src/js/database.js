710255ea1060a2ce43f057e8ca6da914267731d0
Christian Martinez <christian@mixdlabs.com>
2023-09-01 17:00:41 -0600

    Get db function added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,15 +18,25 @@ export const putDb = async (content) => {
     const connection = await openDB('jate', 1)
     const transaction = connection.transaction('jate', 'readwrite')
     const dataStore = transaction.objectStore('jate')
-    const request = dataStore.put({ id: 1, value: content })
-    const result = await request
-    console.log({ message: 'PUT request to jate DB', data: result })
+    const dbData = await dataStore.put({ id: 1, value: content })
+    console.log({ message: 'PUT request to jate DB', data: dbData })
   } catch (err) {
     console.log(err)
   }
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented')
+export const getDb = async () => {
+  try {
+    const connection = await openDB('jate', 1)
+    const transaction = connection.transaction('jate', 'readonly')
+    const dataStore = transaction.objectStore('jate')
+    const dbData = await dataStore.getAll()
+    console.log({ message: 'GET request to jate DB', data: dbData })
+    return dbData ? dbData.value : null
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 initdb()
