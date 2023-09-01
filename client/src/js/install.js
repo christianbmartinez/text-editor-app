4e1bdb51fcabf1be97690b1f81b06d3ba458399d
Christian Martinez <christian@mixdlabs.com>
2023-09-01 17:43:20 -0600

    Install logic, start scripts in package json, changed port in server js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,30 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById('buttonInstall')
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log('beforeinstallprompt fired!')
+  e.preventDefault()
+  window.deferredPrompt = e
+  butInstall.classList.toggle('hidden', false)
+})
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  console.log('butInstall btn click event fired!')
+  const promptEvent = window.deferredPrompt
+  if (!promptEvent) {
+    return
+  }
+  const result = await promptEvent.userChoice
+  console.log(result)
+  promptEvent.prompt()
+  window.deferredPrompt = null
+  butInstall.classList.toggle('hidden', true)
+})
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (e) => {
+  console.log('appinstalled event fired!')
+  window.deferredPrompt = null
+})
